57fd280df045461dbce6d220fbf89316732d1a2e
Alex Browne <stephenalexbrowne@gmail.com>
2015-07-05 20:45:34 -0700

    Add copyright notices

--- rest.go
+++ rest.go
@@ -1,3 +1,7 @@
+// Copyright 2015 Alex Browne.  All rights reserved.
+// Use of this source code is governed by the MIT
+// license, which can be found in the LICENSE file.
+
 // package rest contains functions for sending http requests to a REST API, which
 // can be used to create, read, update, and delete models from a server.
 //
